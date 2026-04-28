c9432050aa8c396fed7703df23c7f8e70fffc47a
Emman Roy Pielago <emmanroy.pielago@cit.edu>
2026-04-28 21:30:01 +0800

    docs: finalize monorepo structure with server files at root

---
+++ server/src/test/java/AppDev/CampusMarketplace/CampusMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package AppDev.CampusMarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
